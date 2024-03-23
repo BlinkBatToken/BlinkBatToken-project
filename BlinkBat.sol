c36e72b7ad886ae2f0b61b942d2f82e8ea694c10
BlinkBatToken <164611260+BlinkBatToken@users.noreply.github.com>
2024-03-23 20:40:24 +0200

    Update BlinkBat.sol

--- BlinkBat.sol
+++ BlinkBat.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BlinkBat is ERC20, ERC20Burnable, ReentrancyGuard, Ownable {
-    uint256 public constant TOTAL_SUPPLY = 66e9 * (10 ** uint256(decimals())); // 66 billion tokens
+    uint256 public constant TOTAL_SUPPLY = 66e9 * 1e18; // 66 billion tokens, assuming 18 decimal places
     address public liquidityWallet;
     address public cexWallet;
     address public teamWallet;
@@ -18,11 +19,12 @@ contract BlinkBat is ERC20, ERC20Burnable, ReentrancyGuard, Ownable {
 
     bool public mintingFinished = false; 
 
-    constructor() ERC20("BlinkBat", "BBT") {
-        _mint(msg.sender, TOTAL_SUPPLY);
-        freezeUntil = block.timestamp + 365 days;
-        frozenAmount = (TOTAL_SUPPLY * 7) / 100;
-    }
+    constructor() ERC20("BlinkBat", "BBT") Ownable(msg.sender) {
+    _mint(msg.sender, TOTAL_SUPPLY);
+    freezeUntil = block.timestamp + 365 days;
+    frozenAmount = (TOTAL_SUPPLY * 7) / 100;
+}
+
 
     function distributeInitialTokens(
         address _liquidityWallet,
@@ -50,12 +52,11 @@ contract BlinkBat is ERC20, ERC20Burnable, ReentrancyGuard, Ownable {
         _transfer(msg.sender, marketingWallet, marketingSupply);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
-        super._beforeTokenTransfer(from, to, amount);
-        if (from == frozenWallet && block.timestamp < freezeUntil) {
+    function _beforeTokenTransfer(address , address, uint256 amount) internal view {
+        if (msg.sender == frozenWallet && block.timestamp < freezeUntil) {
             require(balanceOf(frozenWallet) - amount >= frozenAmount, "BlinkBat: transfer of frozen tokens is restricted");
         }
-    }
+    }   
 
     function mint(address to, uint256 amount) public onlyOwner returns (bool) {
         require(!mintingFinished, "Minting is finished");
@@ -74,13 +75,12 @@ contract BlinkBat is ERC20, ERC20Burnable, ReentrancyGuard, Ownable {
     function safeTransfer(address to, uint256 amount) public nonReentrant {
         _transfer(msg.sender, to, amount);
     }
-    function burn(uint256 amount) public virtual {
-        _burn(_msgSender(), amount);
-    }
-    function burnFrom(address account, uint256 amount) public virtual {
-        _spendAllowance(account, _msgSender(), amount);
-        _burn(account, amount);
-    }
+    function burn(uint256 amount) public override {
+    super.burn(amount);
+}
+    function burnFrom(address account, uint256 amount) public override {
+    super.burnFrom(account, amount);
+}
     function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
         _approve(_msgSender(), spender, allowance(_msgSender(), spender) + addedValue);
         return true;
@@ -93,3 +93,4 @@ contract BlinkBat is ERC20, ERC20Burnable, ReentrancyGuard, Ownable {
         return true;
     }
 }
+
